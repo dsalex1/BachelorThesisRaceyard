d67576869240ddbb42d6d7415bb7621815fd01a4
dsalex1 <dsalex1@ymail.com>
2021-09-14 23:32:55 +0200

    fixed github pages baseURL

--- TrackVisualizerJS/trackvisualizerjs/vue.config.js
+++ TrackVisualizerJS/trackvisualizerjs/vue.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 
 module.exports = {
-    outputDir: path.resolve(__dirname, "../../docs")
-}
+    outputDir: path.resolve(__dirname, "../../docs"),
+    publicPath: process.env.NODE_ENV === "production" ? "/BachelorThesisRaceyard/" : "/",
+};
